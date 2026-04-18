d3547afc422ba07711a1fbb7f1c9677958a5631b
Leo Baldwin-Ramult <mail@leverarchfile.org>
2026-04-18 19:24:29 +1200

    update nvim keymaps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,19 +1,16 @@
 -- files
--- vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
--- vim.api.nvim_set_keymap("n", "FF", ":w!<enter>", {noremap=false})
-
-vim.api.nvim_set_keymap("n", "<leader>fd", ":q!<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "<leader>ff", ":w!<enter>", {noremap=false})
+vim.keymap.set("n", "<leader>fd", ":q!<enter>", {noremap=false})
+vim.keymap.set("n", "<leader>ff", ":w!<enter>", {noremap=false})
 
 -- view marks in Telescope
-vim.api.nvim_set_keymap("n", "MM", ":Telescope marks<enter>", {noremap=false})
+vim.keymap.set("n", "MM", ":Telescope marks<enter>", {noremap=false})
 
 -- buffers
-vim.api.nvim_set_keymap("n", "tj", ":bnext<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "tk", ":bprev<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "th", ":bfirst<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "tl", ":blast<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
+vim.keymap.set("n", "tj", ":bnext<enter>", {noremap=false})
+vim.keymap.set("n", "tk", ":bprev<enter>", {noremap=false})
+vim.keymap.set("n", "th", ":bfirst<enter>", {noremap=false})
+vim.keymap.set("n", "tl", ":blast<enter>", {noremap=false})
+vim.keymap.set("n", "td", ":bdelete<enter>", {noremap=false})
 
 -- new vertical split
 vim.keymap.set("n", "<leader>ws", ":vsplit<CR><C-w>w", {noremap=false})
