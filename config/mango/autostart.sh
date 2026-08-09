60b591f9372d05899177dfef05f947a20cd3cfa3
Leo Baldwin-Ramult <mail@leverarchfile.org>
2026-08-09 16:10:33 +1200

    switch to mango

---
+++ config/mango/autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+systemctl --user restart xdg-desktop-portal
+
+/usr/lib/polkit-kde-authentication-agent-1 &
+
+gsettings set "org.gnome.desktop.interface" gtk-theme "Gruvbox-Material-Dark"
+gsettings set "org.gnome.desktop.interface" icon-theme "breeze-dark"
+
+kanshi &
+waybar &
+mako &
+udiskie &
+foot --server &
+kmonad ~/.config/kmonad/config.kbd &
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+wl-clip-persist --clipboard regular &
+nm-applet &
+librepods --no-tray &
+
+swayidle -w timeout 300 "lock" timeout 600 "wlopm --off '*'" \
+	resume "wlopm --on '*'" before-sleep "lock" &
