45b40dd2d9b97546f221dc298b1770e2877ef1dc
Leo Baldwin-Ramult <mail@leverarchfile.org>
2026-08-30 11:40:32 +1200

    prism gtk4 fixes, remove icons in nvim telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,13 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
 
   config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = { disable_devicons = true },
+        live_grep = { disable_devicons = true },
+        buffers = { disable_devicons = true },
+      },
+    }
 
     vim.keymap.set('n', '<leader>fs', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
